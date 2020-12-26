160a92cc9ac196160fd9fc975d52aab66c9d18fb
fzp <fzp@DESKTOP-HD39HVG>
2020-12-26 17:37:02 +0800

    WindowsForm連線

--- WindowsFormsApp9/Form1.Designer.cs
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -36,6 +36,10 @@
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -68,7 +72,7 @@
             "95無鉛",
             "98無鉛",
             "柴油"});
-            this.comboBox1.Location = new System.Drawing.Point(63, 30);
+            this.comboBox1.Location = new System.Drawing.Point(63, 51);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 23);
             this.comboBox1.TabIndex = 2;
@@ -85,7 +89,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(226, 28);
+            this.textBox1.Location = new System.Drawing.Point(229, 50);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 25);
             this.textBox1.TabIndex = 4;
@@ -93,14 +97,17 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(518, 30);
+            this.comboBox2.Items.AddRange(new object[] {
+            "現金",
+            "信用卡"});
+            this.comboBox2.Location = new System.Drawing.Point(522, 50);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 23);
             this.comboBox2.TabIndex = 5;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(372, 28);
+            this.textBox2.Location = new System.Drawing.Point(374, 49);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 25);
             this.textBox2.TabIndex = 6;
@@ -115,11 +122,51 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "油品";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "點數";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(402, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "下限";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(542, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "付款方式";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.comboBox2);
@@ -147,6 +194,10 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- WindowsFormsApp9/Form1.cs
+++ WindowsFormsApp9/Form1.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ namespace WindowsFormsApp9
 {
     public partial class Form1 : Form
     {
-        static string connectionString = @"Server=localhost;Database=pos_data;User ID=sa;Password=123;";
+        static string connectionString = @"Server=localhost;Database=postgres;User ID=postgres;Password=1234;";
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +23,8 @@ namespace WindowsFormsApp9
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            string SQL = @"select * from car_data";
+            dataGridView1.DataSource = GetAll(SQL);
         }
         static DataTable GetAll(string _SQLCommand)
         {
@@ -41,11 +43,16 @@ namespace WindowsFormsApp9
         static int ExecNonQuery(string _SQLCommand)
         {
             int result = 0;
+            string ans = "",ans2= "", ans3 = "", ans4 = "";
             using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
-            {
+            { 
                 connection.Open();
 
                 NpgsqlCommand cmd = new NpgsqlCommand(_SQLCommand, connection);
+                cmd.Parameters.AddWithValue("@comboBox1",ans);
+                cmd.Parameters.AddWithValue("@textBox1", ans2);
+                cmd.Parameters.AddWithValue("@textBox2", ans3);
+                cmd.Parameters.AddWithValue("@comboBox2", ans4);
                 cmd.CommandType = CommandType.Text;
 
                 result = cmd.ExecuteNonQuery();
@@ -57,7 +64,7 @@ namespace WindowsFormsApp9
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            string SQL = @"select car_no,cust_id from car_data";
+            string SQL = @"select * from car_data";
             Console.WriteLine(GetAll(SQL).Rows.Count);
             foreach(DataRow row in GetAll(SQL).Rows)
             {
@@ -74,7 +81,9 @@ namespace WindowsFormsApp9
 
         private void button3_Click(object sender, EventArgs e)
         {
-            string SQL = "insert into car_data (car_no,cust_id,unit_id,product_id,status,tran_time) values ('q1','w1','e1','r1',1,'t1');";
+            string SQL = @"INSERT INTO car_data 
+                (car_no,cust_id,unit_id,product_id,status,tran_time) 
+                VALUES(1, 1, @comboBox1,@textBox1, 2,@textBox2,@comboBox2) ";
             Console.WriteLine("新增結果:" + ExecNonQuery(SQL));
 
             dataGridView1.Update();
